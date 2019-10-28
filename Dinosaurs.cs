906ec0d7f0ebd20d2b06fadfd052ffe44687af2a
miguelteran1997 <54339141+miguelteran1997@users.noreply.github.com>
2019-10-28 08:24:46 -0500

    Add files via upload

---
+++ Dinosaurs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jurassic.Park.Data
+{
+    public class Dinosaurs
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DID { get; set; }
+        public string DB { get; set; }
+        public string DH { get; set; }
+        public string DN { get; set; }
+     
+
+        //foreign key
+        public int EID { get; set; }
+
+        [ForeignKey("EID")]
+        public Exibits Exi { get; set; }
+
+        public static implicit operator Dinosaurs(List<Dinosaurs> v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Exibits.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jurassic.Park.Data
+{
+    public class Exibits
+    {
+         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EID { get; set; }
+
+        [Display(Name = "Exibits Amount")]
+        public string EAmount { get; set; }
+
+        [Display(Name = "Exibits Name")]
+        public string EName { get; set; }
+    }
+}
